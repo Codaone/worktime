9e19c8e8d165c23c59a338558a6cf4e6eb8d2ea1
Juhani Haapala <Juhani.Haapala@codaone.fi>
2015-04-10 10:59:22 +0300

    Database structure.

---
+++ worktime.sql
@@ -0,0 +1,62 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : codaone.com
+Source Server Version : 50540
+Source Host           : localhost:3306
+Source Database       : worktime
+
+Target Server Type    : MYSQL
+Target Server Version : 50540
+File Encoding         : 65001
+
+Date: 2015-04-10 10:58:38
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user` varchar(32) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `description` text NOT NULL,
+  `group` int(10) NOT NULL DEFAULT '0',
+  `last_ip` varchar(20) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `active` tinyint(3) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for work
+-- ----------------------------
+DROP TABLE IF EXISTS `work`;
+CREATE TABLE `work` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL DEFAULT '1',
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `begin` int(20) NOT NULL,
+  `charged` tinyint(3) NOT NULL DEFAULT '0',
+  `duration` decimal(10,2) DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
+  `starttime` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=741 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for work_payments
+-- ----------------------------
+DROP TABLE IF EXISTS `work_payments`;
+CREATE TABLE `work_payments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
